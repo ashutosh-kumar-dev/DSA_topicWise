bd8d0ec6c05a2fe69466ef8ff360650ab5114bfe
ashutosh kumar <turboblitz.dev@gmail.com>
2023-01-20 22:04:29 +0530

    highlited other methods

--- contains_duplicate.cpp
+++ contains_duplicate.cpp
@@ -1,3 +1,9 @@
+/*
+other methods are:
+1. ek array bana kr index me count karo;
+2. brute force;
+*/
+
 #include<iostream>
 #include<algorithm>
 #include<vector>
@@ -7,7 +13,7 @@ int main(){
 
     vector<int > arr={9,8,5,2,0,22,3,6,5,4};
 
-    sort(arr.begin(),arr.end());
+    sort(arr.begin(),arr.end());  //O(nlogn)
 
     for(int i=0;i<arr.size();i++){
         if(arr.at(i)==arr.at(i+1)) cout<<"contains duplicate"; break;
